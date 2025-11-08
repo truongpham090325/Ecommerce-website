f145da48d577ce7ba71287016bb846a0621b3269
Phạm Quang Trường <truongpham090325@gmail.com>
2025-11-08 08:35:04 +0700

    Khởi tạo dự án

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Website đang chạy trên cổng ${port}`);
+});
