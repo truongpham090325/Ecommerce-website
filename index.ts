44a7615bda8e942e71151f0dcbff44cd08824854
Phạm Quang Trường <truongpham090325@gmail.com>
2025-11-08 10:09:09 +0700

    04. Ghép code Frontend cho Trang chủ (bên client)

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import express, { Request, Response } from "express";
+import path from "path";
 const app = express();
 const port = 3000;
 
+// Thiết lập thư mục views và view engine Pug
+app.set("views", path.join(__dirname, "views")); // Thư mục chứa file Pug
+app.set("view engine", "pug"); // Thiết lập Pug làm view engine
+
+// Thiết lập thư mục chứa file tĩnh
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.render("client/pages/home", {
+    pageTitle: "Trang chủ",
+  });
 });
 
 app.listen(port, () => {
