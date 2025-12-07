d4a322a33e4de8843a48be4494e6129e09c47972
Phạm Quang Trường <truongpham090325@gmail.com>
2025-12-07 11:42:13 +0700

    2.2. Xây dựng hệ thống lưu trữ file độc lập - Ecommerce

--- controllers/admin/file-manager.controller.ts
+++ controllers/admin/file-manager.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import FormData from "form-data";
+import axios from "axios";
+import Media from "../../models/media.model";
 
 export const fileManager = (req: Request, res: Response) => {
   res.render("admin/pages/file-manager", {
@@ -6,10 +9,40 @@ export const fileManager = (req: Request, res: Response) => {
   });
 };
 
-export const uploadPost = (req: Request, res: Response) => {
-  console.log(req.files);
+export const uploadPost = async (req: Request, res: Response) => {
+  try {
+    const files = req.files as Express.Multer.File[];
 
-  res.json({
-    success: true,
-  });
+    const formData = new FormData();
+
+    files?.forEach((file) => {
+      formData.append("files", file.buffer, {
+        filename: file.originalname,
+        contentType: file.mimetype,
+      });
+    });
+
+    const response = await axios.post(
+      "http://localhost:4000/file-manager/upload",
+      formData,
+      formData.getHeaders()
+    );
+    if (response.data.code == "success") {
+      await Media.insertMany(response.data.saveLinks);
+      res.json({
+        code: "success",
+        message: "Upload thành công!",
+      });
+    } else {
+      res.json({
+        code: "error",
+        message: "Lỗi upload!",
+      });
+    }
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Lỗi upload!",
+    });
+  }
 };
--- public/admin/assets/js/uppy.js
+++ public/admin/assets/js/uppy.js
@@ -4,26 +4,25 @@ import {
   XHRUpload,
 } from "https://releases.transloadit.com/uppy/v4.18.2/uppy.min.mjs";
 
-const uppy = new Uppy();
+const uppyUpload = document.querySelector("#uppy-upload");
+if (uppyUpload) {
+  const uppy = new Uppy();
 
-uppy.use(Dashboard, {
-  target: "#uppy-upload",
-  inline: true,
-  width: "100%",
-});
+  uppy.use(Dashboard, {
+    target: "#uppy-upload",
+    inline: true,
+    width: "100%",
+  });
 
-uppy.use(XHRUpload, {
-  endpoint: `/${pathAdmin}/file-manager/upload`, // backend sẽ nhận được file tại link này
-  fieldName: "files",
-  bundle: true,
-});
+  uppy.use(XHRUpload, {
+    endpoint: `/${pathAdmin}/file-manager/upload`, // backend sẽ nhận được file tại link này
+    fieldName: "files",
+    bundle: true,
+  });
 
-uppy.on("complete", (result) => {
-  if (result.successful.length > 0) {
-    drawNotify("success", `Upload thành công ${result.successful.length} file`);
-  }
-  if (result.failed.length > 0) {
-    drawNotify("error", `Upload lỗi ${result.successful.length} file`);
-  }
-  window.location.reload();
-});
+  uppy.on("upload-success", (file, response) => {
+    const res = response.body;
+    drawNotify(res.code, res.message);
+    window.location.reload();
+  });
+}
