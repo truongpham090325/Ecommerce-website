f81e7199f053eda709aeea80de75cd5441505f6b
Phạm Quang Trường <truongpham090325@gmail.com>
2026-01-01 10:18:02 +0700

    2.3. Hiển thị dữ liệu ra Danh sách sản phẩm

--- controllers/client/product.controller.ts
+++ controllers/client/product.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import CategoryProduct from "../../models/category-product.model";
+import Product from "../../models/product.model";
 
 export const productByCategory = async (req: Request, res: Response) => {
   const categoryDetail = await CategoryProduct.findOne({
@@ -13,8 +14,36 @@ export const productByCategory = async (req: Request, res: Response) => {
     return;
   }
 
+  const productList: any = await Product.find({
+    deleted: false,
+    status: "active",
+    category: categoryDetail.id,
+  }).sort({
+    position: "desc",
+  });
+
+  for (const item of productList) {
+    item.discount = Math.floor(
+      ((item.priceOld - item.priceNew) / item.priceOld) * 100
+    );
+
+    // Màu sắc
+    const colorSet = new Set();
+    item.variants
+      .filter((variant: any) => variant.status)
+      .forEach((variant: any) => {
+        variant.attributeValue.forEach((attr: any) => {
+          if (attr.attrType == "color") {
+            colorSet.add(attr.value);
+          }
+        });
+      });
+    item.colorList = [...colorSet];
+  }
+
   res.render("client/pages/product-by-category", {
     pageTitle: categoryDetail.name,
     categoryDetail: categoryDetail,
+    productList: productList,
   });
 };
